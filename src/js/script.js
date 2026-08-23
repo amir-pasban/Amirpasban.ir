ec42eef25f17455652ca7bf9f93ec8fd1ed71c67
AMIR PASBAN <AmirPasban.dev@gmail.com>
2026-08-23 18:03:08 +0330

    update all style  and change the skill

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,29 @@
-
-// skill
-const skillsSection = document.querySelector(".container-skill");
-const bars = document.querySelectorAll("");
+const skillsContainer = document.querySelector(".container-skills");
 
 const observer = new IntersectionObserver((entries) => {
+
     entries.forEach(entry => {
-        if(entry.isIntersecting){
-            bars.forEach(bar=>{
-                bar.style.width =
-                    getComputedStyle(bar).getPropertyValue("--target");
+
+        if (entry.isIntersecting) {
+
+            const skills = skillsContainer.querySelectorAll(".skills");
+
+            skills.forEach(skill => {
+
+                const percent = skill.querySelector(".percent-skill");
+
+                const target = getComputedStyle(percent, "::after")
+                    .getPropertyValue("--target");
+
+                percent.style.setProperty("--current-width", target);
             });
 
-            observer.unobserve(entry.target);
+            observer.unobserve(skillsContainer);
         }
     });
-},{
-    threshold:0.3
+
+}, {
+    threshold: 0.3
 });
 
-observer.observe(skillsSection);
+observer.observe(skillsContainer);
